6d42bec853c41ecb6da58a1d8769949a83047a52
apfelmarmelade <sophie.kallmeier@gmx.de>
2017-03-17 05:35:18 +0100

    Hi

--- behavior/scripts/index.js
+++ behavior/scripts/index.js
@@ -106,7 +106,7 @@ exports.handle = function handle(client) {
       gQ4: 'in_num1/q4',
     },
     streams: {
-      main: 'handleGreeting',
+      main: 'greeting',
       goodbye: handleGoodbye,
       greeting: handleGreeting,
       gQ1: handleQ1,
